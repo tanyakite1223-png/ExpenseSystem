8ea67bfc9b838f89da740c233a5ab99cf50f02c6
Amber <tanyakite1223@gmail.com>
2026-05-25 23:15:35 +0800

    feat:新增Expense狀態欄位及審核流程

--- ExpenseSystem/Migrations/20260525090327_ExpenseIsDelete.Designer.cs
+++ ExpenseSystem/Migrations/20260525090327_ExpenseIsDelete.Designer.cs
@@ -4,6 +4,7 @@ using ExpenseSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpenseSystem.Migrations
 {
     [DbContext(typeof(ExpenseDbContext))]
-    partial class ExpenseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260525090327_ExpenseIsDelete")]
+    partial class ExpenseIsDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -40,7 +43,7 @@ namespace ExpenseSystem.Migrations
                     b.Property<DateTime>("ExpenseDate")
                         .HasColumnType("datetime2");
 
-                    b.Property<bool>("IsDeleted")
+                    b.Property<bool>("ISDeleted")
                         .HasColumnType("bit");
 
                     b.Property<string>("RejectionReason")
