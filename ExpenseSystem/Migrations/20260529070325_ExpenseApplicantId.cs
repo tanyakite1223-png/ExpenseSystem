05fb6c6fbfc5d9a772219e29d5ff917f2542fa25
Amber <tanyakite1223@gmail.com>
2026-05-29 17:06:54 +0800

    feat: Expenses新增欄位、Create()存Id、Index()過濾

---
+++ ExpenseSystem/Migrations/20260529070325_ExpenseApplicantId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpenseApplicantId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicantId",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApplicantId",
+                table: "Expenses");
+        }
+    }
+}
