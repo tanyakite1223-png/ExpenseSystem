d4a6c58f126df5865411a0705b836c853c6ce22c
Amber <tanyakite1223@gmail.com>
2026-06-13 16:36:58 +0800

    feat: model-ExpenseDetail增加Category、StoreName、InvoiceNumber欄位、新增 ExpenseCategory enum(費用分類)

--- ExpenseSystem/Migrations/20260613055603_AlterExpenseDetailFields.Designer.cs
+++ ExpenseSystem/Migrations/20260613055603_AlterExpenseDetailFields.Designer.cs
@@ -4,6 +4,7 @@ using ExpenseSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExpenseSystem.Migrations
 {
     [DbContext(typeof(ExpenseDbContext))]
-    partial class ExpenseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260613055603_AlterExpenseDetailFields")]
+    partial class AlterExpenseDetailFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
