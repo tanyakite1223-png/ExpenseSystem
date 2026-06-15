72b01edaa3ff23428551953848639954002aa600
Amber <tanyakite1223@gmail.com>
2026-06-15 18:48:23 +0800

    feat: 新增 ExpenseDetail 明細建立與 Project 封存功能

--- ExpenseSystem/Models/Project.cs
+++ ExpenseSystem/Models/Project.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ExpenseSystem.Models
 {
     public class Project
     {
         public int ProjectId { get; set; }
+
+        [Display(Name = "專案名稱")]
         public string ProjectName { get; set; }
+
+        [Display(Name = "封存")]
         public bool IsActive { get; set; }
 
         public List<ExpenseDetail>? ExpenseDetails { get; set; }
+
 
     }
 }
