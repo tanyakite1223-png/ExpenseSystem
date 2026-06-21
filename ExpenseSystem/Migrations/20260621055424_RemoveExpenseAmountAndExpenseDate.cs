d5650914c1051d5e17c42aa18301f7087fffeb3c
Amber <tanyakite1223@gmail.com>
2026-06-21 18:37:45 +0800

    refactor: 移除主檔Amount/ExpenseDate,增加明細總額欄

---
+++ ExpenseSystem/Migrations/20260621055424_RemoveExpenseAmountAndExpenseDate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExpenseAmountAndExpenseDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "ExpenseDate",
+                table: "Expenses");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Expenses",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpenseDate",
+                table: "Expenses",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
